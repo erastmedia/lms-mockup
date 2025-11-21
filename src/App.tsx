ddfcdbce14f5f59174f418f901ebb6796da19eac
erastmedia <erastmedia@gmail.com>
2025-11-21 17:37:13 +0700

    chore: comment out unused React imports in App and LmsLayout components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import React from "react";
+// import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import LmsLayout from "./layouts/LmsLayout";
 
